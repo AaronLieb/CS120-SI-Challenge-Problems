cae147f1c3a9c1fad6a3494b845dc198e4059692
AaronLieb <aarnliebmrn@gmail.com>
2020-09-21 15:47:22 -0700

    added round.cpp

---
+++ problems/round.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+/*
+ * You are given a double, x
+ * floor x to the 2nd decimal place using
+ * the operators '*' and '/'.
+ *
+ * print out the result.
+ *
+ * Hint: Integers cannot hold decimals. 
+*/
+
+int main() {
+	double x;
+	cout << "Enter a number: ";
+	cin >> x;
+
+	/* Start your code here */
+
+
+	/* End your code here */
+	
+	return 0;
+}
