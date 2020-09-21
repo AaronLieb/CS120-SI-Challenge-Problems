e6ec37c8ddfafee7b3198844dd413105ee5015bc
AaronLieb <aarnliebmrn@gmail.com>
2020-09-21 15:21:21 -0700

    initial commit

---
+++ problems/quadraticFormula.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <math.h> // imports sqrt function
+using namespace std;
+
+/* Perform the quadratic formula using the doubles a, b, and c.
+ * the variables represent the 3 coefficients of a quadratic equation of the form
+ * ax^2 + bx + c
+ * The equation for the quadratic formula is:
+ * x = (-b +/- sqrt(b^2 - 4ac))/2a
+ *
+ * If both roots exist, print them out
+ * If there is one solution, print it out
+ * If there is no solutions (imaginary), then print out "imaginary solutions"
+*/
+
+int main() {
+	double a, b, c;
+	cout << "Enter the value for a: ";
+	cin >> a;
+	cout << "Enter the value for b: ";
+	cin >> b;
+	cout << "Enter the value for c: ";
+	cin >> c;
+
+	/* Your code starts here */
+
+	/* Your code ends here */
+
+	return 0;
+}
---
+++ problems/switch.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+ * You are given the variables 'a' and 'b'
+ * switch the values stored in the two variables.
+*/
+
+int main() {
+	int a, b;
+	cout << "Enter the values for a: ";
+	cin >> a;
+	cout << "Enter the value for b: ";
+	cin >> b;
+
+	/* Start your code here */
+
+
+	/* End your code here */
+	return 0;
+}
