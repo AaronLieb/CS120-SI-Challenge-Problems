ae8390c204ca2671bf2c3a11eb42cc11a82ceb4e
AaronLieb <aarnliebmrn@gmail.com>
2020-09-21 16:18:04 -0700

    values to value

--- problems/switch.cpp
+++ problems/switch.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 /*
- * You are given the variables 'a' and 'b'
+ * You are given the integers 'a' and 'b'
  * switch the values stored in the two variables.
  * print the values stored in the variables as shown:
  * a: <value for a>
@@ -12,7 +12,7 @@ using namespace std;
 
 int main() {
 	int a, b;
-	cout << "Enter the values for a: ";
+	cout << "Enter the value for a: ";
 	cin >> a;
 	cout << "Enter the value for b: ";
 	cin >> b;
