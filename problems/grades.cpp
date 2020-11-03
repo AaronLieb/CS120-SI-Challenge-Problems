c87a37b9016dc7f10e21235a388a9152f875f3cf
AaronLieb <aarnliebrmn@gmail.com>
2020-11-02 16:35:56 -0800

    done

--- problems/grades.cpp
+++ problems/grades.cpp
@@ -2,18 +2,27 @@
  * Create an enumeration called "grades" that stores the minimum
  * value needed for a student to earn that grade in the class
  *
+ * F: 0-59
+ * D: 60-69
+ * C: 70-79
+ * B: 80-89
+ * A: 90-100
+ *
  * Ask the user to input the grades:
  * Their 1st exam
  * Their 2nd exam
  * And their Final exam 
  *
  * Create a variable representing the average value of the three exams
+ *
  * 
- * Using a switch statement, give the user one of 4 messages:
+ * Using if statements, give the user one of 4 messages:
  * "You failed the class! Better luck next year!"
  * "You You passed the class with a C! Theres some room for improvement."
  * "You passed the class with a D! Great Job!"
  * "You passed the class with an A, Wow! Amazing!"
+ *
+ * Use your grade enumeration
  *
  */
 
